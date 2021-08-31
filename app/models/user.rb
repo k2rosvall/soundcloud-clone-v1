60549f1c36c2bb1a939ff32d6b408fbd22eb94f3
karla rosas <karla.rosas@michelada.io>
2021-08-31 18:21:14 -0500

    Add placeholder for profile photo and css changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_create :set_default_values
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true
-
-  before_create :set_default_values
+  validates :display_name, uniqueness: true
 
   private
 

2fc1c79006826106f7e90bee142245a3b1bcc84d
karla rosas <karla.rosas@michelada.io>
2021-09-01 14:08:11 -0500

    Fix rubocop

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -6,7 +6,7 @@ class ProfileController < ApplicationController
   end
 
   private
-  
+
   def find_user
     user = User.find_by(display_name: params[:display_name])
     redirect_to root_url, notice: "User doesn't exist" if user.nil?

7af6c7555013da02c26015e77f6bc80dffbb82af
karla rosas <karla.rosas@michelada.io>
2021-09-07 17:43:51 -0500

    Add css to tracks view

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -25,7 +25,7 @@ class TracksController < ApplicationController
 
     respond_to do |format|
       if @track.save
-        format.html { redirect_to @track, notice: "Track was successfully created." }
+        format.html { redirect_to profile_path(current_user.profile_id), notice: "Track was successfully created." }
         format.json { render :show, status: :created, location: @track }
       else
         format.html { render :new, status: :unprocessable_entity }

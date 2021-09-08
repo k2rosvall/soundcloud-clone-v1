e5aaf06716d5d6db536d4782e29167357e272371
karla rosas <karla.rosas@michelada.io>
2021-09-08 15:28:45 -0500

    Add tests to track controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TracksController < ApplicationController
+  before_action :set_track, only: %i[edit update destroy]
+
   # GET /tracks/new
   def new
     @track = Track.new
@@ -47,6 +49,11 @@ class TracksController < ApplicationController
   end
 
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_track
+    @track = Track.find(params[:id])
+  end
 
   # Only allow a list of trusted parameters through.
   def track_params
--- test/controllers/tracks_controller_test.rb
+++ test/controllers/tracks_controller_test.rb
@@ -3,7 +3,20 @@
 require 'test_helper'
 
 class TracksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @track = tracks(:one)
+  end
+
+  test 'should get edit' do
+    sign_in users(:one)
+    get edit_track_url(@track)
+    assert_response :success
+  end
+
+  test 'should create track' do
+    sign_in users(:one)
+    get new_track_url
+    assert_response :success
   end
 end

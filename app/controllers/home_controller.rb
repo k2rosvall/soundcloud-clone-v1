f0f3940d3b41bc30877c583444d4e0fecd8a52c3
karla rosas <karla.rosas@michelada.io>
2021-09-14 14:13:10 -0500

    Add tracks_per_genre method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,31 +3,17 @@
 class HomeController < ApplicationController
   def index
     @latest_tracks = latest_tracks_per_user
-    @pop_tracks = pop_tracks
-    @rock_tracks = rock_tracks
-    @edm_tracks = dance_and_edm_tracks
-    @rap_tracks = hip_hop_and_rap_tracks
   end
 
   private
 
   def latest_tracks_per_user
-    User.includes(:tracks).all.map { |u| u.tracks.last }
+    User.includes(:tracks).first(50).map { |u| u.tracks.last }.compact
   end
 
-  def pop_tracks
-    Track.where(genre: 'Pop')
+  def tracks_per_genre(genre)
+    Track.where(genre: genre).first(50)
   end
 
-  def rock_tracks
-    Track.where(genre: 'Rock')
-  end
-
-  def dance_and_edm_tracks
-    Track.where(genre: 'Dance & EDM')
-  end
-
-  def hip_hop_and_rap_tracks
-    Track.where(genre: 'Hip-Hop & Rap')
-  end
+  helper_method :tracks_per_genre
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  GENRES = ['Alternative Rock', 'Ambient', 'Classical',
+            'Country', 'Dance & EDM', 'Dancehall',
+            'Deep House', 'Disco', 'Drum & Bass',
+            'Dubstep', 'Electronic', 'Folk & Singer-Songwriter',
+            'Hip-Hop & Rap', 'Indie', 'Jazz & Blues',
+            'Latin', 'Metal', 'Piano',
+            'Pop', 'R&B & Soul', 'Reggae',
+            'Reggaeton', 'Rock', 'Soundtrack',
+            'Techno', 'Trance', 'Trap',
+            'Triphop', 'World'].freeze
 end

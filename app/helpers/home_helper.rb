d462d7194b851e3f1ba322fa55527ab47913df62
karla rosas <karla.rosas@michelada.io>
2021-09-14 14:18:05 -0500

    Change Genres constant

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,14 +1,7 @@
 # frozen_string_literal: true
 
 module HomeHelper
-  GENRES = ['Alternative Rock', 'Ambient', 'Classical',
-            'Country', 'Dance & EDM', 'Dancehall',
-            'Deep House', 'Disco', 'Drum & Bass',
-            'Dubstep', 'Electronic', 'Folk & Singer-Songwriter',
-            'Hip-Hop & Rap', 'Indie', 'Jazz & Blues',
-            'Latin', 'Metal', 'Piano',
-            'Pop', 'R&B & Soul', 'Reggae',
-            'Reggaeton', 'Rock', 'Soundtrack',
-            'Techno', 'Trance', 'Trap',
-            'Triphop', 'World'].freeze
+  GENRES = ['Alternative Rock', 'Dance & EDM', 'Electronic',
+            'Hip-Hop & Rap', 'Indie', 'Metal',
+            'Pop', 'R&B & Soul', 'Rock'].freeze
 end
